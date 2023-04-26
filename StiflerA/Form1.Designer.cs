d991de79d6f7c7b4d180e3d332bd58937cb95994
Artemilus <KudinovArtem@outlook.com>
2023-04-26 21:52:28 +0600

    Добавьте файлы проекта.

---
+++ StiflerA/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace StiflerA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            StartButton = new Button();
+            StartTextBox = new TextBox();
+            AnswerTextBox = new TextBox();
+            AnswerButton = new Button();
+            AnserLabel = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(382, 330);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // StartButton
+            // 
+            StartButton.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            StartButton.Location = new Point(400, 57);
+            StartButton.Name = "StartButton";
+            StartButton.Size = new Size(318, 25);
+            StartButton.TabIndex = 1;
+            StartButton.Text = "Start";
+            StartButton.UseVisualStyleBackColor = true;
+            StartButton.Click += StartButton_Click;
+            // 
+            // StartTextBox
+            // 
+            StartTextBox.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            StartTextBox.Location = new Point(400, 12);
+            StartTextBox.Multiline = true;
+            StartTextBox.Name = "StartTextBox";
+            StartTextBox.Size = new Size(318, 28);
+            StartTextBox.TabIndex = 2;
+            // 
+            // AnswerTextBox
+            // 
+            AnswerTextBox.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            AnswerTextBox.Location = new Point(400, 272);
+            AnswerTextBox.Multiline = true;
+            AnswerTextBox.Name = "AnswerTextBox";
+            AnswerTextBox.Size = new Size(318, 28);
+            AnswerTextBox.TabIndex = 4;
+            // 
+            // AnswerButton
+            // 
+            AnswerButton.Location = new Point(400, 317);
+            AnswerButton.Name = "AnswerButton";
+            AnswerButton.Size = new Size(318, 23);
+            AnswerButton.TabIndex = 3;
+            AnswerButton.Text = "Дать ответ";
+            AnswerButton.UseVisualStyleBackColor = true;
+            AnswerButton.Click += AnswerButton_Click;
+            // 
+            // AnserLabel
+            // 
+            AnserLabel.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            AnserLabel.Location = new Point(400, 85);
+            AnserLabel.Name = "AnserLabel";
+            AnserLabel.RightToLeft = RightToLeft.No;
+            AnserLabel.Size = new Size(318, 184);
+            AnserLabel.TabIndex = 5;
+            AnserLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(730, 362);
+            Controls.Add(AnserLabel);
+            Controls.Add(AnswerTextBox);
+            Controls.Add(AnswerButton);
+            Controls.Add(StartTextBox);
+            Controls.Add(StartButton);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button StartButton;
+        private TextBox StartTextBox;
+        private TextBox AnswerTextBox;
+        private Button AnswerButton;
+        private Label AnserLabel;
+    }
+}
---
+++ StiflerA/Form1.cs
@@ -0,0 +1,101 @@
+namespace StiflerA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public string text;
+        public char[] word;
+        public int hp;
+        public int hptrue;
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            text = StartTextBox.Text;
+            hp = 0;
+            AnserLabel.Text = null;
+            StartTextBox.Text = null;
+            word = new char[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                AnserLabel.Text += '.';
+                word[i] = '.';
+            }
+
+        }
+
+        private void AnswerButton_Click(object sender, EventArgs e)
+        {
+            bool words = false;
+            int j = 0;
+            if (AnswerTextBox.Text.Length == 1)
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (AnswerTextBox.Text[0] == text[i])
+                    {
+                        words = true;
+                        hptrue++;
+                        word[i] = text[i];
+                        AnserLabel.Text = null;
+                    }
+                }
+                if (words == true)
+                {
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        AnserLabel.Text += word[i];
+
+                    }
+                    if (hptrue == text.Length)
+                    {
+                        MessageBox.Show("Ты победил!");
+                    }
+                }
+                else
+                {
+                    hp++;
+                    string? hpbuff = hp.ToString() + ".png";
+                    pictureBox1.Image = new Bitmap(hpbuff);
+                    if (hp == 10)
+                    {
+                        MessageBox.Show("Ты проиграл!");
+                        AnserLabel.Text = null;
+                        AnserLabel.Text = text;
+
+
+                    }
+                }
+            }
+            else if (AnswerTextBox.Text.Length > 1)
+            {
+                if (AnswerTextBox.Text == text)
+                {
+                    AnserLabel.Text = null;
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        AnserLabel.Text += text[i];
+                    }
+                    MessageBox.Show("Ты победил");
+
+                }
+                else
+                {
+                    hp = 10;
+                    string? hpbuff = hp.ToString() + ".png";
+                    pictureBox1.Image = new Bitmap(hpbuff);
+                    MessageBox.Show("Ты проиграл");
+                    AnserLabel.Text = null;
+                    AnserLabel.Text = text;
+                }
+            }
+            AnswerTextBox.Text = null;
+        }
+    }
+}
